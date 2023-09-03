8d261b30e00b2a5a99cc3b15a0bc236b162c26e4
HassanSohail-2001 <muhammadhassan@fastechsystem.co>
2023-09-03 21:17:36 +0500

    #1 update codebase

--- Controllers/EmailController.js
+++ Controllers/EmailController.js
@@ -46,7 +46,6 @@ const callBack = async (req, res) => {
   
     oauth2Client.setCredentials(tokens)
 
-    // res.redirect(`/emails?access_token=${tokens.access_token}&refresh_token=${tokens.refresh_token}&expiry_date=${formattedExpiryDate}`);
 
 
   
